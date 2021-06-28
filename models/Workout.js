c78b6192bcb57ffccb8e21081f470093c71bec16
Byungwook Yoon <byung90@gmail.com>
2021-06-28 04:27:01 -0400

    set up

---
+++ models/Workout.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now()
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: 'Type name is required.'
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: 'Name of exercise is required.'
+      },
+      duration: {
+        type: Number,
+        min: 1
+      },
+      weight: {
+        type: Number,
+        min: 1
+      },
+      reps: {
+        type: Number,
+        min: 1
+      },
+      sets: {
+        type: Number,
+        min: 1
+      }
+    }
+  ]
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+module.exports = Workout;
